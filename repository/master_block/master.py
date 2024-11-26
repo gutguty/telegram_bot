717b983d79291cf3eaefd3a980a15928e89822e4
Guty <bogdan.gutyy@mail.ru>
2024-11-26 20:41:52 +0300

    Project start

---
+++ repository/master_block/master.py
@@ -0,0 +1,5 @@
+class Master:
+    def __init__(self,mode):
+        print("INIT MASTER")
+        #self.mode = mode
+        print(mode)
---
+++ repository/start.py
@@ -0,0 +1,4 @@
+from master_block.master import Master
+
+if __name__ == "__main__":
+    master = Master("Engineer")
